204440010ec3a23fcab61218989a503e459cc9ff
Joe Zhou <chn.challenger@gmail.com>
2016-05-20 22:00:53 +0100

    some amendments

--- acronom.rb
+++ acronom.rb
@@ -1,19 +1,14 @@
 def make_acronym(phrase)
-
     letters = ("a".."z").to_a << " "
-
     if phrase.is_a? String
-
       for i in 0...phrase.length
           unless letters.include?(phrase.downcase[i])
               return "Not letters"
           end
       end
-
       array = phrase.split(" ")
       array = array.map!{|x| x[0].upcase}.join("")
       return array
-
     else
         return "Not a string"
     end
