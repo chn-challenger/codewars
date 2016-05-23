ddf0cfabfc421200ff32d5fc67d07bfae7202835
Joe Zhou <chn.challenger@gmail.com>
2016-05-23 23:18:41 +0100

    amend kata

--- arrayMadnessK8.js
+++ arrayMadnessK8.js
@@ -1,7 +1,7 @@
 function arrayMadness(a,b) {
   console.log(a);
     console.log(b);
-  var val_a = a.reduce((x,y)=>x+y*y,1);
-  var val_b = b.reduce((x,y)=>x+y*y,1);
+  var val_a = a.reduce((x,y)=>x+y*y,0);
+  var val_b = b.reduce((x,y)=>x+y*y*y,0);
  return val_a > val_b;
 }
