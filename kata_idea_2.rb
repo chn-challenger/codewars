03687e3fd92806038a781a829aa1a8dfe2ad634e
Joe Zhou <chn.challenger@gmail.com>
2016-05-24 22:52:17 +0100

    new kata solvable minesweep

---
+++ kata_idea_2.rb
@@ -0,0 +1,7 @@
+def solvable_minesweeper(map)
+
+end
+
+#given a mine sweepermap, determine whether or not it is solvable,
+#a map is solvable if it has at least one squire starting from which will allow
+#the rest to be logically deduced.
